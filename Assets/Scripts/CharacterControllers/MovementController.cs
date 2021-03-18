3f29d90811b7908c71cd6251b3d2f7791587041d
nickmesh <59099009+nickmesh@users.noreply.github.com>
2021-03-18 15:28:59 -0700

    Created WASD and Arrow Key movement
    Created the MoveControl_nam scene with a capsule and plane in order to test the MovementController Script. Created the MovementController Script and used it to allow for basic WASD and arrow key movement.

---
+++ Assets/Scripts/CharacterControllers/MovementController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public CharacterController controller;
+    public bool CanMove;     
+    public int speed = 1;
+    public GameObject AnimController;
+    public void move()
+    {
+        //Gets inputs from the players wasd or arrow keys
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        //calculates direction to move based on inputs
+        Vector3 moveDirection = new Vector3(horizontal, 0f, vertical).normalized;
+
+        //moves the player if move keys are pressed and CanMove is true
+        if (moveDirection.magnitude >= 0.1f)
+        {
+            if (CanMove)
+            {
+                controller.Move(moveDirection * speed * Time.deltaTime);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+}
