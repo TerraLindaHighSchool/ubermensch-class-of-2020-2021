b07f1422a132e45c2539c55f3cb6c93e4062910f
vivianjohnson <59099112+vivianjohnson@users.noreply.github.com>
2021-03-19 17:13:56 -0700

    Inventory Interface Script Started

---
+++ Assets/Scripts/InventoryItemInterface.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItemInterface : MonoBehaviour
+{
+    interface IInventoryItem
+    {
+        string name { get; set; }
+        int value { get; set; }
+        GameObject icon {get; set;}
+        string toolTip { get; set; }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
