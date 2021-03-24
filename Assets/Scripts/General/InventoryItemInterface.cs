24725283925eb2eea0ae4212efb45e4b33e78b84
vivianjohnson <59099112+vivianjohnson@users.noreply.github.com>
2021-03-24 12:28:30 -0700

    Inventory Item Interface Complete

--- Assets/Scripts/General/InventoryItemInterface.cs
+++ Assets/Scripts/General/InventoryItemInterface.cs
@@ -2,30 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public interface IInventoryItem
-{
-    string Name { get; set; }
-    int Value { get; set; }
-    //GameObject Icon { get; set; }
-    string ToolTip { get; set; }
-}
-
-
-public class FakeInventoryItem : IInventoryItem
-{
-    public string Name { get; set; } 
-    public int Value { get; set; }
-    // public GameObject icon;
-    public string ToolTip { get; set; }
-
-    public FakeInventoryItem(string itemName, int itemValue, string itemToolTip)
+namespace InventoryItem
+{ 
+    interface IInventoryItem
     {
-        Name = itemName;
-        Value = itemValue;
-        ToolTip = itemToolTip;
-    }
-
-    FakeInventoryItem GasMask = new FakeInventoryItem("Gas Mask", 20, "Can be equiped to ward off fumes");
+         string Name { get; set; }
+         int Value { get; set; }
+         GameObject Icon { get; set; }
+         string ToolTip { get; set; }
+        
+    }   
 
 }
