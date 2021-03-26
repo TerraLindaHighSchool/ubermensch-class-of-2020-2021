b80606b5dfcc8bf0ce24bada7f4ca8bbf50eaae7
BEANZEXTREME <59098911+BEANZEXTREME@users.noreply.github.com>
2021-03-26 14:10:15 -0700

    created dialogue tree
    did that and moved statement to general

---
+++ Assets/Scripts/General/DialogueTree.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace DialogueTree
+{
+    interface DialogueTree
+    {
+        float relationshipType { get; set; }
+
+        Statement[] conversationPoints { get; } 
+    }
+}
