b5a36486438515bcf90e399db5e4db66d0242ca7
BEANZEXTREME <59098911+BEANZEXTREME@users.noreply.github.com>
2021-03-26 14:40:13 -0700

    added dialogue controller
    yeah that's as far as I know

---
+++ Assets/Scripts/General/DialogueController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueController : MonoBehaviour
+{
+
+    public DialogueTree conversation;
+    public DialogueTree combat;
+    private int currentposition;
+
+    public void LoadNext(int Option)
+    {
+        //I think it's supposed to keep trak of the options?
+
+    }
+
+    public void StartConversation()
+    {
+        //plays the opener?
+    }
+
+    public void StartCombat()
+    {
+        //plays the other opener?
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
