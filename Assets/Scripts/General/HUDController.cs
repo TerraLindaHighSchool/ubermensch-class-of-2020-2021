81e012e079040cf1b135493111a331ebc93e04c5
nickmesh <59099009+nickmesh@users.noreply.github.com>
2021-03-26 15:00:05 -0700

    HUDControllerClass started
    I began making the script for the HUDController class. However, I am not completely sure if it works properly and will check with Finlay/Lucas.

---
+++ Assets/Scripts/General/HUDController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDController : MonoBehaviour
+{
+    private GameObject activeHUD;
+    public GameObject[] HUDs;
+    public void HUDLoader(int hud, GameObject caller)
+    {
+        HUDs[hud].SetActive(true);
+    }
+    public void HUDDeLoader(int hud)
+    {
+        HUDs[hud].SetActive(false);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
