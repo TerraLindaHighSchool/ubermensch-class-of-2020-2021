bcc3a7e4d127c6af583f51d45c03b3603d11bb22
BEANZEXTREME <59098911+BEANZEXTREME@users.noreply.github.com>
2021-03-26 13:48:15 -0700

    this is the way
    apparently this is what I was supposed to do

--- Assets/Scripts/statement.cs
+++ Assets/Scripts/statement.cs
@@ -2,46 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Statement : MonoBehaviour
+namespace Statement
 {
-    public string opener;
-    public string OptionOne;
-    public float OptionOneModifier;
-    public Statement OptionOneOutcome;
-    //public (+etc) OptionTwo;
-
-    public void Set(float item, string StringSet, Statement StatementSet)
+    interface Statement
     {
-        switch (item)
-        {
-            case 0:
-
-             break;
-            case 1:
-
-                break;
-            case 1.3:
-
-                break;
-            case 1.6:
-
-                break;
-            /*case etc
-
-            break;*/
-        }
-
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        string opener { get; }
+        string OptionOne { get; }
+        float OptionOneModifier { get; }
+        Statement OptionOneOutcome { get; }
     }
 }
+
