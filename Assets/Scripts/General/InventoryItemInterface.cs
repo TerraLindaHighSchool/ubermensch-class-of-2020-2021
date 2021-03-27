f081be864ceecca86cef3c67d7cc550bb9ff332f
vivianjohnson <59099112+vivianjohnson@users.noreply.github.com>
2021-03-26 20:54:30 -0700

    InventoryManager in testing

--- Assets/Scripts/General/InventoryItemInterface.cs
+++ Assets/Scripts/General/InventoryItemInterface.cs
@@ -12,5 +12,74 @@ namespace InventoryItems
         GameObject Icon { get; }
         string ToolTip { get; }
         
-    }  
+    }
+
+    class InventoryManager
+    {
+        int numSoap;
+        int numFood;
+        int numShield;
+        int numJewelry;
+        int numMedicine;
+        int numShiv;
+        
+        ArrayList inventoryItem = new ArrayList();
+
+        public void AddItem(InventoryItem item)
+        {
+            inventoryItem.Add(item);
+        }
+
+        public void RemoveItem(InventoryItem item)
+        {
+            inventoryItem.Remove(item);
+        }
+
+        // if what is printed should to be the number of items in the inventory:
+        // need to add it to the HUD once it's made 
+        public int PrintInventory()
+        {
+            return inventoryItem.Capacity; 
+        }
+
+        // if what is printed should to be the name of the items in the inventory:
+        // need to add to the HUD once it's made - that shouold be simple though
+        // Specific items can be added (like a gas mask, but the specific ones weren't given) 
+        public string PrintInventoryItem()
+        {
+            string inventorySummary;
+
+            foreach(InventoryItem itm in inventoryItem)
+            {
+                if(itm.Name.Equals("Soap"))
+                {
+                    numSoap++; 
+                }
+                if(itm.Name.Equals("Food"))
+                {
+                    numFood++; 
+                }
+                if (itm.Name.Equals("Shield"))
+                {
+                    numShield++; 
+                }
+                if(itm.Name.Equals("Jewelry"))
+                {
+                    numJewelry++; 
+                }
+                if(itm.Name.Equals("Medicine"))
+                {
+                    numMedicine++; 
+                }
+                if(itm.Name.Equals("Shiv"))
+                {
+                    numShiv++; 
+                }
+
+            }
+            inventorySummary = "Soap: " + numSoap + "Food: " + numFood + "Shields: " + numShield + "Jewelry: " + numJewelry + "Medicine" + numMedicine + "Shiv" + numShiv;
+            return inventorySummary; 
+        }
+
+    }
 }
