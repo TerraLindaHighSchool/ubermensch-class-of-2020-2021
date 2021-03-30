3f4bbc0612866d005cc2bc82a215ffb3f2b4a2e2
nickmesh <59099009+nickmesh@users.noreply.github.com>
2021-03-30 15:32:59 -0700

    MusicController Started
    I started the MusicController script. However, there is still a lot to do, and only a basic outline of the script and its methods have been created.

---
+++ Assets/Scripts/General/MusicController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public AudioClip[] tracks;
+   /* 
+    public AudioClip Switch(int trackIndex)
+    {
+        AudioSource currentSong = GetComponent<AudioSource>();
+        currentSong.clip = tracks[trackIndex];
+        currentSong.Play();
+    }
+    */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
