b40713a70374eeeba85305eed9bb5849f2cb83a3
vivianjohnson <59099112+vivianjohnson@users.noreply.github.com>
2021-03-30 16:11:52 -0700

    InventoryManager is complete

--- Assets/Scripts/General/InventoryItemInterface.cs
+++ Assets/Scripts/General/InventoryItemInterface.cs
@@ -2,106 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace InventoryItems
-{ 
-    interface InventoryItem
+public class InventoryItemInterface
+{
+    public interface InventoryItem
     {
 
         string Name { get; }
         int Value { get; }
-       // GameObject Icon { get; }
+        GameObject Icon { get; }
         string ToolTip { get; }
-        
+
     }
-    class FakeInventoryItem : InventoryItem
+
+}
+
+public class InventoryManager : InventoryItemInterface
+{
+    public string inventoryNames; 
+    ArrayList inventoryItem = new ArrayList();
+
+    public void AddItem(InventoryItem item, ArrayList inventoryItem)
     {
-        public string Name { get; }
-        public int Value { get; }
-        public string ToolTip { get; }
-
-        public FakeInventoryItem(string itemName, int itemValue, string itemTip)
-        {
-            Name = itemName;
-            Value = itemValue;
-            ToolTip = itemTip;
-        }
-
-        
+        inventoryItem.Add(item);
     }
 
-    class InventoryManager
+    public void RemoveItem(InventoryItem item, ArrayList inventoryItem)
     {
-        int numSoap;
-        int numFood;
-        int numShield;
-        int numJewelry;
-        int numMedicine;
-        int numShiv;
-        
-        ArrayList inventoryItem = new ArrayList();
-
-        public void AddItem(InventoryItem item)
-        {
-            inventoryItem.Add(item);
-        }
-
-        public void RemoveItem(InventoryItem item)
-        {
-            inventoryItem.Remove(item);
-        }
-
-        // if what is printed should to be the number of items in the inventory:
-        // need to add it to the HUD once it's made 
-        public int PrintInventory()
-        {
-            return inventoryItem.Capacity; 
-        }
-
-        // if what is printed should to be the name of the items in the inventory:
-        // need to add to the HUD once it's made - that shouold be simple though
-        // Specific items can be added (like a gas mask, but the specific ones weren't given) 
-        public string PrintInventoryItem()
-        {
-            string inventorySummary;
-
-            foreach(InventoryItem itm in inventoryItem)
-            {
-                if(itm.Name.Equals("Soap"))
-                {
-                    numSoap++; 
-                }
-                if(itm.Name.Equals("Food"))
-                {
-                    numFood++; 
-                }
-                if (itm.Name.Equals("Shield"))
-                {
-                    numShield++; 
-                }
-                if(itm.Name.Equals("Jewelry"))
-                {
-                    numJewelry++; 
-                }
-                if(itm.Name.Equals("Medicine"))
-                {
-                    numMedicine++; 
-                }
-                if(itm.Name.Equals("Shiv"))
-                {
-                    numShiv++; 
-                }
-
-            }
-            inventorySummary = "Soap: " + numSoap + "Food: " + numFood + "Shields: " + numShield + "Jewelry: " + numJewelry + "Medicine" + numMedicine + "Shiv" + numShiv;
-            return inventorySummary; 
-        }
-
-        public void Start()
-        {
-            FakeInventoryItem Soap = new FakeInventoryItem("Soap", 20, "Spend Money");
-            inventoryItem.AddItem(Soap);
-        }
-
+        inventoryItem.Remove(item);
     }
-       
+
+     // if what is printed should to be the number of items in the inventory:
+     // need to add it to the HUD  
+     public string PrintInventory(ArrayList inventoryItem)
+     {
+        foreach (InventoryItem itm in inventoryItem)
+        {
+            inventoryNames += itm.Name + ", ";
+        }
+        if(inventoryItem.Capacity == 0)
+        {
+            inventoryNames = "Inventory is empty"; 
+        }
+        return inventoryNames;  
+     }     
 }
