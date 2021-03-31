59fb6064bfe257e4ba82ce9a0d47e17c09bac5b5
nickmesh <59099009+nickmesh@users.noreply.github.com>
2021-03-31 15:09:04 -0700

    SFXController Complete
    I created the SFX controller script, which creates an array for the sound effects. It also returns a requested sound effect, and returns an error if the sound effect does not exist.

---
+++ Assets/Scripts/General/SFXController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXController : MonoBehaviour
+{
+    public AudioClip[] sfx;
+
+    public AudioClip effectPlay(int soundIndex)
+    {
+        if(soundIndex <= sfx.Length)
+        {
+            return sfx[soundIndex];
+        }
+        else
+        {
+            Debug.Log("uh oh missing sound effectso");
+            return null;
+        }
+    }
+}
