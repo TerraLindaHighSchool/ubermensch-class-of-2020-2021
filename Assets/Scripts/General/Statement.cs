4c301ea25a740ff5fa5c2845a1dbb8cb591719a2
BEANZEXTREME <59098911+BEANZEXTREME@users.noreply.github.com>
2021-04-02 12:52:12 -0700

    comments
    so i remember whats going on lol

--- Assets/Scripts/General/Statement.cs
+++ Assets/Scripts/General/Statement.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Statement
 {
-
+    //what the npc says
     public string NpcLine;
+    //what you say
     public string[] Response;
+    //how much it changes the relationshhip type
     public float[] ResponseModifier;
+    //I think it's which one it sends you to
     public int[] ResponseOutcome;
     
     //I think these were supposed to be different but they look wrong(too small)
