0279c854a2c853851e6ef81ea31161b5297f444e
DavidShalit <59098962+DavidShalit@users.noreply.github.com>
2021-04-14 15:33:40 -0700

    Started follower manager

---
+++ Assets/Scripts/General/FollowerManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowerManager : MonoBehaviour
+{
+    public FollowerInterface.Follower[] followers = new FollowerInterface.Follower[40];
+
+    public void AddFollower(GameObject avatar)
+    {
+
+    }
+
+    public void RemoveFollower(string name)
+    {
+
+    }
+
+  /*  public InventoryItemInterface.InventoryItem[] PrintFollowers()
+    {
+        
+    }*/
+
+}
