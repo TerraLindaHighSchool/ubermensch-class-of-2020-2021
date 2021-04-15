17fa10b75b95a2a6f585904ac9e68363f13f2e5a
vivianjohnson <59099112+vivianjohnson@users.noreply.github.com>
2021-04-15 11:28:21 -0700

    Player Controllertasks 1-3 complete

--- Assets/Scripts/CharacterControllers/PlayerController.cs
+++ Assets/Scripts/CharacterControllers/PlayerController.cs
@@ -4,19 +4,48 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public int health { get;  private set; }
+    public int health { get; set; }
+    // What is the data type for food and oxygen? 
+    public bool food { get; set; }
+    public bool oxygen { get; set; }
     public bool isInDialogue { get; private set; }
     public bool isInCombat { get; private set; }
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // what should the initail values be? 
+        health = 10;
+        food = true;
+        oxygen = true; 
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Friendly NPC"))
+        {
+            isInDialogue = true; 
+           // Debug.Log("in conversation with friendly NPC"); 
+        }
+        if(other.CompareTag("Non-Friendly NPC"))
+        {
+            isInCombat = true;
+           // Debug.Log("In combat with non-friendly NPC"); 
+        }
+        if(other.CompareTag("Inventory Object"))
+        {
+            if(Input.GetKey(KeyCode.E))
+            {
+                // pick up and put in inventory
+                // how to call the add to inventory method in this class 
+            }
+        }
     }
 }
