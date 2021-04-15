65c0af00aa5ee82fb757fa0428e9fff02b34e297
nickmesh <59099009+nickmesh@users.noreply.github.com>
2021-04-15 15:34:53 -0700

    HUDController Script Changed
    I added multiple methods to the HUDController Script, in order to allow for dialogue. However, none of them are fully functional, as they require the DialogueController and buttons on the conversation HUD.

--- Assets/Scripts/General/HUDController.cs
+++ Assets/Scripts/General/HUDController.cs
@@ -6,16 +6,49 @@ public class HUDController : MonoBehaviour
 {
     private int activeHUD;
     public GameObject[] HUDs;
+    public bool inConvo = false;
     //Disables the previously actived hud, activates the new hud, 
     //and sets the new hud to be the active hud
+
     public void HUDLoader(int hud, GameObject caller)
     {
-        if(hud<= HUDs.Length)
+        if(hud< HUDs.Length)
         {
             HUDs[activeHUD].SetActive(false);
             activeHUD = hud;
             HUDs[activeHUD].SetActive(true);
         }
+    }
+
+    public void HUDLoader(int hud, GameObject caller, GameObject Npc)
+    {
+        if (hud < HUDs.Length)
+        {
+            HUDs[activeHUD].SetActive(false);
+            activeHUD = hud;
+            HUDs[activeHUD].SetActive(true);
+            if (activeHUD == 0)
+            {
+                /*
+                Npc.StartConversation();
+                inConvo = true;
+                */
+            }
+        }
+    }
+
+    public void continueConvo(int buttonNumber, GameObject Npc)
+    {
+        if(inConvo)
+        {
+            /*
+             * Npc.LoadNext(buttonNumber)
+             */
+        }
+    }
+    private void printDialogue(GameObject Npc)
+    {
+
     }
     //Disables the active hud
     public void HUDDeLoader(int hud)
