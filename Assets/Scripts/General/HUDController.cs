4a66669cae00486f9c1ddf7cd00a7fed8cf7f5f3
nickmesh <59099009+nickmesh@users.noreply.github.com>
2021-04-16 14:58:15 -0700

    HUDController Update 4/16
    Added integration with DialogueController, fixed some small errors.

--- Assets/Scripts/General/HUDController.cs
+++ Assets/Scripts/General/HUDController.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/* HUD 0 IS CONVERSATION 
+ * HUD 1 IS COMBAT
+ * HUD 2 IS INVENTORY
+ */
 public class HUDController : MonoBehaviour
 {
     private int activeHUD;
     public GameObject[] HUDs;
     public bool inConvo = false;
+    public GameObject activeNpc;
     //Disables the previously actived hud, activates the new hud, 
     //and sets the new hud to be the active hud
-
     public void HUDLoader(int hud, GameObject caller)
     {
         if(hud< HUDs.Length)
@@ -20,6 +23,7 @@ public class HUDController : MonoBehaviour
         }
     }
 
+    //Same as HUDLoader, but also sets the active Npc and starts a conversation if necessary
     public void HUDLoader(int hud, GameObject caller, GameObject Npc)
     {
         if (hud < HUDs.Length)
@@ -27,29 +31,28 @@ public class HUDController : MonoBehaviour
             HUDs[activeHUD].SetActive(false);
             activeHUD = hud;
             HUDs[activeHUD].SetActive(true);
+            activeNpc = Npc;
             if (activeHUD == 0)
             {
                 /*
-                Npc.StartConversation();
                 inConvo = true;
+                activeNpc.GetComponent<DialogueController>().StartConversation();
                 */
             }
         }
     }
 
-    public void continueConvo(int buttonNumber, GameObject Npc)
+    //Called on button press to continue the conversation
+    public void continueConvo(int buttonNumber)
     {
         if(inConvo)
         {
             /*
-             * Npc.LoadNext(buttonNumber)
+             activeNpc.GetComponent<DialogueController>().LoadNext(buttonNumber);
              */
         }
     }
-    private void printDialogue(GameObject Npc)
-    {
 
-    }
     //Disables the active hud
     public void HUDDeLoader(int hud)
     {
