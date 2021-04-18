ab63ea5a666a779fee6d2c6f3fc4dff08f2db659
Bruce Gustin <bgustin@srcs.org>
2021-04-18 15:24:31 -0700

    Add comments into SwitchCamera.cs

--- Assets/Scripts/General/switchCamera.cs
+++ Assets/Scripts/General/switchCamera.cs
@@ -7,8 +7,16 @@ public class switchCamera : MonoBehaviour
 {
     public GameObject isoCamera;
     public GameObject dialogueCamera;
+    
+    // NPC is set by the playerController whenever the player triggers an NPC collider
     public GameObject NPC { private get; set; }
+
+    /** isInDialogue is set by the playerController to true when the player is inside of 
+    * a NPC trigger and the player presses the E key and to false when the player exits
+    * a NPC trigger.
+    */
     public bool isInDialogue { private get; set; }
+
 
 
     // Update is called once per frame
@@ -17,8 +25,12 @@ public class switchCamera : MonoBehaviour
 
         if (isInDialogue && !dialogueCamera.activeInHierarchy)
         {
+            /* LookAt is set by the playerController to true when the player is inside of 
+            *  a NPC trigger and the player presses the E key
+            */  
             dialogueCamera.GetComponent<CinemachineVirtualCamera>().LookAt = NPC.transform;
             Debug.Log(NPC.name);
+            
             isoCamera.SetActive(false);
             dialogueCamera.SetActive(true);
 
