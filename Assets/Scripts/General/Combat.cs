b55caceb90d32e68b74c3ce29ee11016e5d25b50
BEANZEXTREME <59098911+BEANZEXTREME@users.noreply.github.com>
2021-04-20 15:13:45 -0700

    started combat class
    added the variable things and stuff

---
+++ Assets/Scripts/General/Combat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Combat : MonoBehaviour
+{
+
+    public int npcHealth;
+
+    //finlay said this might go somewhere else
+    //public int npcDamage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
