9ccf98d1e944eef7fc2b86e27b10bf83fd708225
Bruce Gustin <bgustin@srcs.org>
2021-04-30 16:52:43 -0700

    Update Player Prefab with Camera Target

--- Assets/Scripts/General/CameraTrackerController.cs
+++ Assets/Scripts/General/CameraTrackerController.cs
@@ -1,25 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class CameraTrackerController : MonoBehaviour
 {
 	public GameObject player;
-	public GameObject cameraTracker;
 	
 
     // Start is called before the first frame update
     void Start()
     {
-        cameraTracker.transform.rotation = Quaternion.Euler(Vector3.up);
+        transform.rotation = Quaternion.Euler(Vector3.up);
+        SceneManager.activeSceneChanged += RotateCameraTargetOnSceneChange;
     }
 
     // Update is called once per frame
     void Update()
     {
-        cameraTracker.transform.position = player.transform.position + new Vector3(0,0.5f,0);
+        transform.position = player.transform.position + new Vector3(0,0.5f,0);
+    }
 
+    private void OnDisable()
+    {
+        SceneManager.activeSceneChanged -= RotateCameraTargetOnSceneChange;
+    }
+
+    
+    private void RotateCameraTargetOnSceneChange(Scene current, Scene next)
+    {
+        transform.rotation = Quaternion.Euler(Vector3.up);
     }
 
 }
