b2f8e628ecb8d8992082c40f57d2737d31fb360e
katherinenemits <kn00772@srcs.org>
2021-04-30 14:40:21 -0700

    CameraTrackerController Complete

--- Assets/Scripts/General/CameraTrackerController.cs
+++ Assets/Scripts/General/CameraTrackerController.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CameraTrackerController : MonoBehaviour
 {
 	public GameObject player;
 	public GameObject cameraTracker;
-	public bool rotation = false;
+	
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cameraTracker.transform.rotation = Quaternion.Euler(Vector3.up);
     }
 
     // Update is called once per frame
@@ -19,16 +20,6 @@ public class CameraTrackerController : MonoBehaviour
     {
         cameraTracker.transform.position = player.transform.position + new Vector3(0,0.5f,0);
 
-	setRotation();
-	
     }
 
-	void setRotation()
-    {
-	if(rotation == false)
-	{
-	    cameraTracker.transform.rotation = player.transform.rotation;
-	    rotation = true;
-	}
-    }
 }
