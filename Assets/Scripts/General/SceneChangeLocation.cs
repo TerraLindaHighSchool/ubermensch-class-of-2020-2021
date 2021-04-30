234f7a4d79d96d46da04d4b9e8ed51b7c601adb4
WylieDahlgren <59098915+WylieDahlgren@users.noreply.github.com>
2021-04-30 14:35:51 -0700

    Completed SceneChangeLocation and SceneChangeOnTrigger

---
+++ Assets/Scripts/General/SceneChangeLocation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Media;
+using UnityEngine;
+
+public class SceneChangeLocation : MonoBehaviour
+{
+    public string sceneTarget;
+    public Vector3 destinationPosition;
+    public gameObject player;
+    
+    public void teleport()
+    {
+        player.gameObject.position = destinationPosition;
+    }
+}
---
+++ Assets/Scripts/General/SceneChangeOnTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Policy;
+using UnityEngine;
+
+public class SceneChangeOnTrigger : MonoBehaviour
+{
+    public SceneChangeLocation scl;
+    public gameObject player;
+
+    public void onTriggerEnter(Collider other)
+    {
+        if(other == player)
+        {
+            scl.Teleport();
+        }
+    }
+}
