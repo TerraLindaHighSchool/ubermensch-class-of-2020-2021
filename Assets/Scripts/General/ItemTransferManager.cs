becf5a29cd0a1a8aeff3ebbe472e653047d6046e
DavidShalit <59098962+DavidShalit@users.noreply.github.com>
2021-05-05 15:11:32 -0700

    Added ItemTransferManager script

---
+++ Assets/Scripts/General/ItemTransferManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemTransferManager : MonoBehaviour
+{
+    public static GameObject selectedButton;
+    public GameObject selectedButtonID;
+
+    public bool Transfer(InventoryManager sender, InventoryManager reciever, InventoryItemInterface item)
+    {
+        return true;
+    }
+}
