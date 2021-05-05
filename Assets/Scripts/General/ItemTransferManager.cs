80333049ee34a34701455503beb827876452f32d
DavidShalit <59098962+DavidShalit@users.noreply.github.com>
2021-05-05 15:11:50 -0700

    removed fields

--- Assets/Scripts/General/ItemTransferManager.cs
+++ Assets/Scripts/General/ItemTransferManager.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class ItemTransferManager : MonoBehaviour
 {
-    public static GameObject selectedButton;
-    public GameObject selectedButtonID;
-
     public bool Transfer(InventoryManager sender, InventoryManager reciever, InventoryItemInterface item)
     {
         return true;
