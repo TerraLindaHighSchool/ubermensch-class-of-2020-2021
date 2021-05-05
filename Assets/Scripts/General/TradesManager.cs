ef9608aede94fb1e5ab5fea15639013163c65ee6
DavidShalit <59098962+DavidShalit@users.noreply.github.com>
2021-05-05 10:42:08 -0700

    added ideas in comments

--- Assets/Scripts/General/TradesManager.cs
+++ Assets/Scripts/General/TradesManager.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class TradesManager : MonoBehaviour
 {
     public int itemCost;
+    public InventoryItemInterface itemTraded;
     public void UpdateUI()
     {
-
+        itemCost = itemTraded.Value;
     }
 
     public void CheckOut()
     {
+        //if(player.soap >= itemCost)
+        //{
+        //      InventoryManager.AddItem(itemTraded);
+        //      player.soap -= itemCost
+        //}
+    }
 
+    public void SellItem()
+    {
+        //InventoryManager.RemoveItem(itemTraded);
+        //player.soap += itemCost;
     }
 }
