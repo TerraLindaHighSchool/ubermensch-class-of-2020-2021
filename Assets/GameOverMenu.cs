07b1ef611f7f3db5be2d8b88c6bf000da498d521
katherinenemits <kn00772@srcs.org>
2021-05-11 15:37:27 -0700

    Menus and Buttons created

---
+++ Assets/GameOverMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        Debug.Log("Scene Reloaded");
+	SceneManager.LoadScene(SceneManager.GetActiveScene());
+    }
+}
