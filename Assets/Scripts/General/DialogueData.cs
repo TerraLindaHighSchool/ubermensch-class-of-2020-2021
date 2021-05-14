555d72d8713b4c21d51cb0a24baf2821e77fbc20
Bruce Gustin <bgustin@srcs.org>
2021-05-13 22:43:23 -0700

    Preparation for Training Phoebe on this Task

---
+++ Assets/Scripts/General/DialogueData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueData : DialogueTree
+{
+    /*
+     * Picks up Relationship Type, CVS file, and Go Away string from inspector
+     * of NPC object and adds it to the dialogue tree.
+     */
+}
