43868a7a721ac880c5da24b8f9123c48285bb2b9
vivianjohnson <59099112+vivianjohnson@users.noreply.github.com>
2021-05-24 11:44:53 -0700

    script for NPC movement complete

--- Assets/Scripts/CharacterControllers/NPCMoveAI.cs
+++ Assets/Scripts/CharacterControllers/NPCMoveAI.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class NPCAIMove : MonoBehaviour
+public class NPCMoveAI : MonoBehaviour
 {
     public NavMeshAgent navMeshAgent;
     public Transform[] waypoints;
-    public Animator animController;
-    public GameObject player;
+   // public Animator animController;
+    private GameObject player;
     private int currentWaypointIndex;
     private bool isAtWaypoint;
     private const float SPEED = 2.0f;
@@ -18,32 +18,58 @@ public class NPCAIMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        // start walking animation
+        SetMotion(SPEED, ANGULAR_SPEED);
+        navMeshAgent.SetDestination(waypoints[0].position);
+        player = GameObject.Find("PlayerModel"); 
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        CheckIfAtWaypoint();
+        CheckIfNearPlayer(); 
     }
 
     private void CheckIfAtWaypoint()
     {
-
+        if(navMeshAgent.stoppingDistance > navMeshAgent.remainingDistance)
+        {
+            StartCoroutine(WaitAtWaypoint());
+            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+            navMeshAgent.SetDestination(waypoints[currentWaypointIndex].position);
+        }
     }
 
-    private void CheckIfInConversationOrNearPlayer()
+    private void CheckIfNearPlayer()
     {
-
+        Vector3 vectorToPlayer = player.transform.position - transform.position;
+        if (vectorToPlayer.magnitude < 2)
+        {
+            SetMotion(0, 0);
+            Quaternion directionToPlayer = Quaternion.Euler(0, 180 / Mathf.PI * Mathf.Atan2(vectorToPlayer.x, vectorToPlayer.z), 0);            transform.rotation = Quaternion.Slerp(transform.rotation, directionToPlayer, 5.0f * Time.deltaTime);
+        }else if(! isAtWaypoint)
+        {
+            SetMotion(SPEED, ANGULAR_SPEED); 
+        }
     }
 
     private void SetMotion(float speed, float angularSpeed)
     {
-
+        navMeshAgent.speed = speed;
+        navMeshAgent.angularSpeed = angularSpeed; 
     }
 
-    //private IEnumerator WaitAtWaypoint()
-    //{
-
-    //}
+    private IEnumerator WaitAtWaypoint()
+    {
+        //run idle animation before yield
+        // animController.SetBool("isWalking", false); 
+        isAtWaypoint = true;
+        SetMotion(0, 0); 
+        yield return new WaitForSeconds(WAIT_TIME);
+        // start walking animation
+        // animController.SetBool("isWalking", true);
+        isAtWaypoint = false;
+        SetMotion(SPEED, ANGULAR_SPEED);
+    }
 }
