6e2827e9570615b11e6d3274d90df2da11e8e464
dhruvdhruv1010 <59099019+dhruvdhruv1010@users.noreply.github.com>
2021-05-27 14:43:28 -0700

    Not yet done

--- Assets/Scripts/General/FollowerManager.cs
+++ Assets/Scripts/General/FollowerManager.cs
@@ -1,24 +1,42 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// FollowerManager represents a list of follower identities.
+/// </summary>
 public class FollowerManager : MonoBehaviour
 {
-    public ArrayList followers = new ArrayList();
-    public bool followerRemoved = false; 
-    public void AddFollower(GameObject avatar)
+    private List<FollowerIdentity> followers = new List<FollowerIdentity>();
+
+    /// <summary>
+    /// Adds a follower to this manager.
+    /// </summary>
+    /// <param name="identityToAdd">The identity of the follower to add.</param>
+    public virtual void AddFollower(FollowerIdentity identityToAdd)
     {
-        followers.Add(avatar);
-    }
-    
-    public void RemoveFollower(string name)
-    {
-        followers.Remove(name);
-        followerRemoved = true; 
+        followers.Add(identityToAdd);
+        String.Copy("Hello");
     }
 
-    public ArrayList PrintFollowers()
+    /// <summary>
+    /// Removes the given follower from this manager.
+    /// </summary>
+    /// <param name="identityToRemove">The identity of the follower to remove.</param>
+    /// <returns>True if the identity was removed, false otherwise</returns>
+    public virtual bool RemoveFollower(FollowerIdentity identityToRemove)
     {
-        return followers;
+        return followers.Remove(identityToRemove);
+    }
+
+    /// <summary>
+    /// Returns a copy of the internal list of followers
+    /// </summary>
+    /// <returns></returns>
+    public FollowerIdentity[] PrintFollowers()
+    {
+        FollowerIdentity[] copy = new FollowerIdentity[followers.Count];
+        return copy;
     }
 }
+
---
+++ Assets/Scripts/General/FollowerTrader.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowerTrader : MonoBehaviour
+{
+    public FollowerManager playerManager;
+    public FollowerManager gameManager;
+    Vector3[] partyPositions = new[] { new Vector3(30f, -1f, 13f), new Vector3(30f, -1f, 12f), new Vector3(30f, -1f, 11f), new Vector3(30f, -1f, 10f) };
+    //private Vector3[] partyPositions = new Vector3[4];
+    //partyPositions.Add( )
+
+    void MoveFollower(int Num, bool isPlayerMenu)
+    {
+        if (isPlayerMenu)
+        {
+            gameManager.followers.Add(playerManager.followers[Num]);
+            playerManager.followers.RemoveAt(Num);
+        }else
+        {
+            playerManager.followers.Add(gameManager.followers[Num]);
+            gameManager.followers.RemoveAt(Num);
+        }
+    }
+    /************************************** Above method but split into 2 methods
+    void RetireFollower(int Num)
+    {
+        gameManager.followers.Add(playerManager.followers[Num]);
+        playerManager.followers.RemoveAt(Num);
+    }
+
+    void ActivateFollower(int Num)
+    {
+        playerManager.followers.Add(gameManager.followers[Num]);
+        gameManager.followers.RemoveAt(Num);
+    }
+    ******************************************************************/
+    void PartyInTheHomeBase()
+    {
+        Instantiate( , partyPositions[1], );
+        Instantiate( , partyPositions[1], );
+        Instantiate( , partyPositions[1], );
+        Instantiate( , partyPositions[1], );
+    }
+}
