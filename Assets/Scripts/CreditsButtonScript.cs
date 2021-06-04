1ea5fe77974665f04cf920be237fa2243f1e4a40
katherinenemits <kn00772@srcs.org>
2021-06-04 14:48:44 -0700

    Credits Button made

---
+++ Assets/Scripts/CreditsButtonScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsButtonScript : MonoBehaviour
+{
+    public void ShowCredits()
+	{
+	Debug.Log("Credits Displayed");
+	SceneManager.LoadScene("Credits");
+	}
+}
